db94f3823cfa0aaa4059aa58077df20f2148da6c
Florian Jung <flo@windfisch.org>
2019-03-31 20:45:59 +0200

    test. too slow

--- minimax.py
+++ minimax.py
@@ -7,6 +7,7 @@ cache = {}
 
 # minimizes for player -1, maximizes for player 1
 def minimax(gamestate, player_idx):
+	if len(cache) % 1000 == 0: print(len(cache))
 	if gamestate in cache:
 		return cache[gamestate]
 
@@ -18,10 +19,10 @@ def minimax(gamestate, player_idx):
 	best = None
 	for column in range(7):
 		result = gamestate.drop(column, player_idx)
-		print(gamestate)
-		print("->")
-		print(result)
-		print(40*"-")
+		#print(gamestate)
+		#print("->")
+		#print(result)
+		#print(40*"-")
 		if result is not None:
 			result_val = minimax(result, -player_idx) * 0.9 # prioritize fast wins
 			if best is None or best < player_idx * result_val:
@@ -33,4 +34,12 @@ def minimax(gamestate, player_idx):
 
 print("testing minimax")
 
-print( minimax(GameState(), -1) )
+
+gs = GameState()
+gs.drop(1,-1)
+gs.drop(2,-1)
+gs.drop(3,-1)
+gs.drop(2,1)
+gs.drop(3,1)
+gs.drop(4,1)
+print( minimax(gs, -1) )
