3d2070fe47bb3fb0eb3aca19998baa0b98daef20
Mathias 'Flameout' Hackenberg <flameout@ai-arena.de>
2019-03-31 16:11:05 +0200

    CHANGED - pre-filled list

--- GameState.py
+++ GameState.py
@@ -2,6 +2,12 @@
 def generateEmptyGameState():
 	state = []
 	for i in range(7):
-		state.append([])
+		state.append(6*[0])
 	return state
 
+#def didSomebodyWin(gameState):
+#	for column in range(7):
+#		for row in range(6):
+			#Vertical, up:
+			
+			
