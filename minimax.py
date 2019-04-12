75db4ee8e8b1a8f656de886dc493033f5bdd28a3
Florian Jung <flo@windfisch.org>
2019-04-13 01:55:57 +0200

    minimax_bfs

--- minimax.py
+++ minimax.py
@@ -3,11 +3,11 @@ import sys
 
 sys.setrecursionlimit(99999)
 
-cache = {}
-
 # minimizes for player -1, maximizes for player 1
-def minimax(gamestate, player_idx):
-	if len(cache) % 1000 == 0: print(len(cache))
+def minimax(gamestate, player_idx, recursion_limit, cache):
+	if recursion_limit < 0:
+		return None
+
 	if gamestate in cache:
 		return cache[gamestate]
 
@@ -17,6 +17,7 @@ def minimax(gamestate, player_idx):
 		return winner
 
 	best = None
+	recursion_limited = False
 	for column in range(7):
 		result = gamestate.drop(column, player_idx)
 		#print(gamestate)
@@ -24,22 +25,42 @@ def minimax(gamestate, player_idx):
 		#print(result)
 		#print(40*"-")
 		if result is not None:
-			result_val = minimax(result, -player_idx) * 0.9 # prioritize fast wins
-			if best is None or best < player_idx * result_val:
-				best = player_idx * result_val
-	if best is None: best = 0
+			result_val = minimax(result, -player_idx, recursion_limit-1, cache)
+			if result_val is not None:
+				fitness = player_idx * result_val
+				if best is None or best < fitness:
+					best = fitness
+			else:
+				recursion_limited = True
 	
-	cache[gamestate] = best
-	return best
+	if best is None: best = 0
+
+	if recursion_limited and best <= 0:
+		return None
+	else:
+		cache[gamestate] = 0.5 * best * player_idx
+		return 0.5 * best * player_idx
 
 print("testing minimax")
 
 
 gs = GameState()
-gs.drop(1,-1)
-gs.drop(2,-1)
-gs.drop(3,-1)
-gs.drop(2,1)
-gs.drop(3,1)
-gs.drop(4,1)
-print( minimax(gs, -1) )
+gs.drop_inplace(3,-1)
+gs.drop_inplace(4,-1)
+gs.drop_inplace(4,-1)
+gs.drop_inplace(3,-1)
+gs.drop_inplace(0,1)
+gs.drop_inplace(1,1)
+gs.drop_inplace(0,1)
+
+
+def minimax_bfs(gamestate, player_idx, limit):
+	cache = {}
+	for i in range(limit+1):
+		result = minimax(gamestate, player_idx, i, cache)
+		if result != None:
+			return result
+	return None
+	
+
+print( minimax_bfs(gs, -1, 3) )
